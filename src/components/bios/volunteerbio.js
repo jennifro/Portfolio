e3fce0f99502fde23432f03abe15f878ab9f7b5a
Jennifer <jen.dxon@gmail.com>
2019-04-25 13:25:23 -0700

    Added biographical text.

---
+++ src/components/bios/volunteerbio.js
@@ -0,0 +1,6 @@
+const VolunteerBio = `I spent four years as a community organizer in Chicago & Stockton for local organizations comprised of primarily service workers, farm workers & others low-income people. It was a mutual aid system of food & clothing distributions and free preventive medical & dental services. As a full-time volunteer for four years, I worked alongside other low-income members in getting food donations from grocers & wholesalers, monetary donations from local individuals & businesses to pay the office rent & utilities, and recruiting other volunteers from the membership and the community at large. I also helped organize several fundraiser benefits, two anniversary dinners, and the production of a few issues of the newsletter & sponsor's guide.
+
+I have also had the opportunity to coach the Mock Trial team at my alma mater Tracy High School, helping them go to state competition in 2010-2011.`;
+
+
+export default VolunteerBio;
