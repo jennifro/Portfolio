4ecbe0fd022724f90f0864f72da6f43cb9880ead
Jennifer <jen.dxon@gmail.com>
2019-06-10 22:09:16 -0700

    Added gifs of projects.

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import BloomSample from './bloom-sample.gif';
+import FeelingPrezSample from './feeling-prez-sample.gif';
+
 import './projects.css';
 
 
@@ -9,11 +12,13 @@ export default class ProjectContainer extends Component {
       <div id="projects">
       <h4 className="title">Projects</h4>
         <div className="project-container" id="discover-bloom">
+          <img src={BloomSample} alt="Sample of language-learning web app, Bloom, programmed by Jennifer Dixon." />
           <a href="https://app.discoverbloom.com/">
             Bloom, an indigenous language-learning web app currently offering one level with four chapters in Tsalagi (Cherokee).
           </a>
         </div>
         <div className="project-container" id='feelin-presidential' >
+          <img src={FeelingPrezSample} alt="Sample of natural language processing project created by Jennifer Dixon." />
           <a href="https://feeling-prez.herokuapp.com/">
             Feeling Presidential, a sentiment analysis project with
             Python's Natural Language Toolkit (NLTK), Flask & d3.js
