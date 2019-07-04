5e950952609855a67268d363100dd4428769e5ac
Jennifer <jen.dxon@gmail.com>
2019-07-04 15:24:45 -0700

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Jennifer Dixon's Portfolio
-
-***
-made with [Create React App](https://github.com/facebookincubator/create-react-app)
--- src/components/bios/personalbio.js
+++
@@ -1,3 +0,0 @@
-const PersonalBio = `I am a Black, bisexual cis woman who grew up in the Central Valley of California & am happy to call the Bay Area my current home. When I'm not working, I enjoy going to concerts or theater performances with friends, talking walks around my neighborhood & reading fantasy/science fiction with a cat on my lap.`;
-
-export default PersonalBio;
